7d55915e5659c7edf262c1ddc8c48fdd1ed26b77
João Matheus Bueno <jmatheus_05@hotmail.com>
2021-05-08 15:00:37 -0300

    Implementing requests from the front with the API

--- Front/main.js
+++ Front/main.js
@@ -1,16 +1,57 @@
-console.log("Foi")
+let userRequest;
 
-function UserGet(url)
+console.log("main.js Ok")
+
+function Connection()
+{   
+    //Visual Studio URL
+    return "https://localhost:44375/v1/users"
+    
+    //Visual Studio Code URL
+    //return "https://localhost:5001/v1/users"
+}
+
+function Login(){
+    console.log("Login() ok")
+    let url = Connection()
+    let username = document.getElementById('username').value
+    let password = document.getElementById('password').value 
+
+}
+
+function Get(url)
 {
+    console.log("Get() ok")
     let request = new XMLHttpRequest();
     request.open("GET", url, false);
     request.send();
     return request.responseText
 }
 
-function Main()
-{
-    console.log(UserGet("https://localhost:44375/v1/users"))
+function CreateAccount(){
+    console.log("CreateAccount ok")
+    let url = Connection()
+    let username = document.getElementById('username').value
+    let password = document.getElementById('password').value 
+
+    body = {
+        "Username" : username,
+        "Password" : password
+    }
+
+    Post(url, body)
 }
 
-Main()
+function Post(url, body){
+    console.log("Post ok")
+    let request = new XMLHttpRequest()
+    request.open("POST", url, true)
+    request.setRequestHeader("Content-type", "application/json")
+    request.send(JSON.stringify(body))
+
+    request.onload = function() {
+        console.log(this.responseText)
+    }
+
+    return request.responseText
+}
